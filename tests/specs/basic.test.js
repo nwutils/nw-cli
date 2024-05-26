b4fc22902a463b54f219878f9992e85abb5cce1a
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-05-26 16:41:13 +0530

    chore(test): fix file path to nw project

--- tests/specs/basic.test.js
+++ tests/specs/basic.test.js
@@ -18,22 +18,18 @@ describe('basic app', async function () {
   let chromedriverPath = undefined;
 
   beforeAll(async () => {
-
-  }, Infinity);
-
-  it('renders Hello World text', async () => {
     const options = new chrome.Options();
     const args = [
-      `--nwapp=${path.resolve('test', 'fixtures', 'basic')}`,
+      `--nwapp=${path.resolve('tests', 'fixtures', 'basic')}`,
       '--headless=new',
     ];
     options.addArguments(args);
-
     chromedriverPath = await nw.findpath('chromedriver', { flavor: 'sdk' });
-
     const service = new chrome.ServiceBuilder(chromedriverPath).build();
-
     driver = chrome.Driver.createSession(options, service);
+  });
+
+  it('renders Hello World text', async () => {
     const text = await driver.findElement(selenium.By.id('test')).getText();
     expect(text).toEqual('Hello, World!');
   });
