94502e4d968990d492f44a2f7dde0a195abb88bc
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-17 22:32:04 +0530

    chore(test): coverage for nw.App.filteredArgv

--- tests/specs/nw/app.test.js
+++ tests/specs/nw/app.test.js
@@ -54,7 +54,7 @@ describe('nw.App', { timeout: Infinity }, async function () {
 
   it('renders nw.App.fullArgv', async () => {
     /**
-     * `nw.App.argv` looks similar to the following:
+     * `nw.App.fullArgv` looks similar to the following:
      * 
      * `"--allow-pre-commit-input,--disable-popup-blocking,--enable-automation,--enable-logging,--headless=new,--nwapp=/home/ayushmanchhabra/Git/nwutils/cli/tests/fixtures/nw/app,--remote-debugging-port=0,--user-data-dir=/tmp/.io.nwjs.Jb5Vnk"`
      * 
@@ -75,6 +75,27 @@ describe('nw.App', { timeout: Infinity }, async function () {
     expect(argv).toContain('--nwapp=');
     expect(argv).toContain('--remote-debugging-port=');
     expect(argv).toContain('--user-data-dir=');
+  });
+
+  it('renders nw.App.filteredArgv', async () => {
+    /**
+     * `nw.App.filteredArgv` looks similar to the following:
+     * 
+     * `"/^--url=/,/^--remote-debugging-port=/,/^--renderer-cmd-prefix=/,/^--nwapp=/"`
+     * 
+     * @type {string}
+     */
+    const argv = await driver.findElement(selenium.By.id('nw-app-filteredargv')).getText();
+    
+    /* There should be 4 command line arguments. */
+    const argvLength = argv.split(',').length;
+    expect(argvLength).toBe(4);
+    
+    /* Since order does not matter, the `.toContain` matcher is used.*/
+    expect(argv).toContain('--url=');
+    expect(argv).toContain('--remote-debugging-port=');
+    expect(argv).toContain('--renderer-cmd-prefix=');
+    expect(argv).toContain('--nwapp=');
   });
 
   afterAll(async function () {
