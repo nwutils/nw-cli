43a5eb3bd2e94dc8e93974c69dde3fa74c1b70ba
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-08-25 11:01:22 +0530

    fest(nw): add isDevToolsOpen polyfill

---
+++ src/polyfill.js
@@ -0,0 +1,29 @@
+/**
+ * Query the status of devtools window..
+ * 
+ * @function
+ * @param {(status: boolean) => void} callback - Callback function
+ * @returns {void}
+ */
+function isDevToolsOpen(callback) {
+    chrome.windows.getAll({
+        populate: true,
+        windowTypes: ['devtools']
+    },
+    function (wins) {
+        callback(wins.length > 0);
+    })
+}
+
+/**
+ * Polyfill function for missing NW.js functionality.
+ * 
+ * The environment is checked before adding the relevant functions.
+*/
+export default function polyfill () {
+    if (process.versions['nw-flavor'] === 'sdk' && typeof nw.Window.isDevToolsOpen !== 'function') {
+        nw.Window.isDevToolsOpen = isDevToolsOpen;
+        global.nw.Window.isDevToolsOpen = isDevToolsOpen;
+        window.nw.Window.isDevToolsOpen = isDevToolsOpen;
+    }
+}
