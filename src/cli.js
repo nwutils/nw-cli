a54d2abd060b61ef12ee85cb1783b224481bdf31
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-03-25 19:26:13 +0530

    chore(cli): create project via cli

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,20 @@
-export {};
+#!/usr/bin/env node
+
+import process from 'node:process';
+import { program } from 'commander';
+import { create } from '../src/commands/create.js';
+
+program
+    .name('nw')
+    .description('NW project CLI');
+
+program
+    .command('create')
+    .argument('<name>', 'app name')
+    .option('-t, --template <template>', 'template name', 'vanilla-js')
+    .option('-o, --outDir <dir>', 'output directory', '.')
+    .action((name, options) => {
+        create(name, options.template, options.outDir);
+    });
+
+program.parse(process.argv);
